b1b9f627cd7b9507619e7bd756dbdac26ae612ba
akryeziu <https://wwwin.bitbucket.com/>
2024-02-20 15:09:05 +0100

    Fetching Data

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-  
+// Unsplash API
+const count = 10;
+const apiKey = "ySCtuFPH3Frib-AcgWT4JSuNpHB5JW85lFZqlEX-iY0";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try{
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        // Catch Error here
+        // console.log(error);
+    }
+}
+
+
+// On Load
+getPhotos();
